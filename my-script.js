12e3a8954db510341c85e12a54f6828e540e9343
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-24 17:07:12 +0100

    Random string working

--- my-script.js
+++ my-script.js
@@ -1,3 +1,7 @@
 function getComputerChoice() {
+    let myArray = ["Rock", "Paper", "Scissors"];
+    let myValue = myArray[Math.floor(Math.random() * myArray.length)]; // A random number between 0 and myArray.length is generated. Math.floor generates the nearest integer value generated from Math.random. 
+    console.log(myValue);                                              // This random index is used to return a random value from myArray.
     
 }
+getComputerChoice();
