8b2c7f49471ccc2c1038e2f59c0c5b9895f71f1a
Thomas Fogarty <t.fogarty24@outlook.com>
2022-07-25 17:28:03 +0100

    5 round game incomplete

--- my-script.js
+++ my-script.js
@@ -1,32 +1,48 @@
+
 // Function to randomly return either "Rock", "Paper" or "Scissors" as the computers choice.
 function getComputerChoice() {
     const myArray = ["rock", "paper", "scissors"];
     const myValue = myArray[Math.floor(Math.random() * myArray.length)]; // A random number between 0 and myArray.length is generated. Math.floor generates the nearest integer value generated from Math.random.  
-    return myValue;                                                       
+    return myValue;                                                  
 }
 
-
-// Function to play a round of "Rock", "Paper", "Scissors".
+// Function to play a round of "Rock", "Paper", "Scissors". 
 function playRound(playerSelection, computerSelection) { 
   if (
     (playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')
-    ) {alert(`You win! ${playerSelection} beats ${computerSelection}.`)
-    return "Player Win";}
+    ) { alert(`You win! ${playerSelection} beats ${computerSelection}.`);
+        playerScore++;
+        return "Player Win";}
     
     else if(
     (playerSelection == 'rock' && computerSelection == 'paper') ||
     (playerSelection == 'paper' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'rock')
-    ) {alert(`You lose, ${computerSelection} beats ${playerSelection}.`)
-    return "Computer Win";}
+    ) { alert(`You lose, ${computerSelection} beats ${playerSelection}.`);
+        computerScore--;
+        return "Computer Win";} 
 
     else (playerSelection == computerSelection)
         alert(`It's a tie, ${playerSelection} and ${computerSelection} are equal.`)
         return "Tie";
 }  
-
 const playerSelection = prompt("Choose either 'Rock', 'Paper' or 'Scissors'.").toLowerCase();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        if (playerScore == 5){
+            return "Winner!"
+        } else if (computerScore == 5){
+            return "Loser!"
+        }
+    }
+    console.log(playerScore)
+}
+
